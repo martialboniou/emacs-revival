74425d3e54e18119dfec3568a3e49df149337ab6
Martial <hondana@gmx.com>
2011-10-21 16:41:38 +0200

    del / bksp / paredit at work (unstable) > emacs

--- paredit-viper-compat.el
+++ paredit-viper-compat.el
@@ -1,10 +1,11 @@
 ;;; paredit-viper-compat.el: viper-mode modifications to support paredit
 ;;
-;; Copyright (c) 2009, Jeremy L. Rayman.
+;; Copyright (c) 2009, Jeremy L. Rayman. 2011, Martial Boniou
 ;; Author: Jeremy Rayman
+;; Maintainer: Martial Boniou
 ;; URL:  http://www.emacswiki.org/emacs/paredit-viper-compat.el
 ;; Site: http://www.emacswiki.org/emacs/ParEditViperCompat
-;; Status: Beta - not fully tested.
+;; Status: Beta - partially tested version for github/martialboniou/Dots.git .
 
 ;; This mode allows paredit to work when viper is enabled.
 ;;
@@ -45,7 +46,8 @@
 ;;                                      ))))
 ;;
 ;; Changelog:
-;; 0.1 - 2009-08-15 - Initial release.
+;; 0.1a - 2011-10-20 - Fix.
+;; 0.1  - 2009-08-15 - Initial release.
 ;;
 ;; TODO:
 ;; * XEmacs compatibility - paredit uses a variable `paredit-forward-delete-keys'
@@ -57,6 +59,7 @@
 ;;
 ;;
 ;; Copyright (c) 2009, Jeremy L. Rayman
+;;               2011, Martial Boniou
 ;;
 ;; Redistribution and use in source and binary forms, with or without
 ;; modification, are permitted provided that the following conditions
@@ -86,8 +89,8 @@
 ;; NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 ;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-
 (require 'paredit)
+(require 'viper)
 
 ;;; Paredit built-in keys:
 
@@ -102,6 +105,7 @@
     ("\\" . paredit-backslash)
     ; Viper Fixes
     ([(backspace)] . paredit-backward-delete)
+    ([(kp-delete)] . paredit-forward-delete)
     ("\"" . paredit-doublequote))
   "Paredit keys to be available in insert mode only.
 
@@ -110,7 +114,7 @@
 
 (defconst paredit-viper-compat-all-states-keys
       `(; Basic Insertion Commands (most are in a different map though)
-        ([(meta ?\;)] . paredit-comment-dwim)
+    ;    ([(meta ?\;)] . paredit-comment-dwim)
         ; Deleting & Killing:
         ; xemacs users note: the two lines below have
         ; hardcoded values for <delete> which need to be changed for
@@ -120,12 +124,16 @@
         ; to look at `paredit-commands' for usage of the
         ; `paredit-forward-delete-keys' variable.
         ("\C-j" . paredit-newline)
-        ("\C-d <delete>" . paredit-forward-delete)
-        ("\C-d <deletechar>" . paredit-forward-delete)
-        (,(format "<%s>" paredit-backward-delete-key) . paredit-backward-delete)
+        ;(,(format "<%s>" paredit-backward-delete-key) . paredit-backward-delete)
+        ;(,(format "\M-%s" paredit-backward-delete-key) . paredit-backward-kill-word)
+ ;;       ([(control-d delete)] . paredit-forward-delete)
+        ;;("\C-d <deletechar>" . paredit-forward-delete)
+       ;; ([] . paredit-forward-delete)
+;;        ([(control kp-delete)] . paredit-backward-kill-word)
+        ([(control backspace)] . paredit-backward-kill-word)
+        ([(control kp-delete)] . paredit-forward-kill-word)
         ("\C-k" . paredit-kill)
         ("\M-d" . paredit-forward-kill-word)
-        (,(format "\M-%s" paredit-backward-delete-key) . paredit-backward-kill-word)
         ("\C-\M-f" . paredit-forward)
         ("\C-\M-b" . paredit-backward)
         ("\C-\M-p" . backward-down-list)
@@ -134,38 +142,29 @@
         ("\M-(" . paredit-wrap-round)
         ("\M-s" . paredit-splice-sexp)
         ("\M-<up>" . paredit-splice-sexp-killing-backward)
-        ("ESC <up>" . paredit-splice-sexp-killing-backward)
         ("\M-<down>" . paredit-splice-sexp-killing-forward)
-        ("ESC <down>" . paredit-splice-sexp-killing-forward)
         ; Barfage and Slurpage
         ([(control ?\))] . paredit-forward-slurp-sexp)
-        ([(control right)] . paredit-forward-slurp-sexp)
-         ;"\C-<right>" . paredit-forward-slurp-sexp)
-        ([(control ?\})] . paredit-forward-barf-sexp)
-                                        ;"\C-}" . paredit-forward-barf-sexp)
-        ([(control left)] . paredit-forward-barf-sexp)
-                                        ;"\C-<left>"
         ([(control ?\()] . paredit-backward-slurp-sexp)
-                                        ;"\C-(" . paredit-backward-slurp-sexp)
-        ([(control meta left)] . paredit-backward-slurp-sexp)
-        ;; ([(escape control left)] . paredit-backward-slurp-sexp)
+        ([(control ?\})] . paredit-forward-barf-sexp)
         ([(control ?\{)] . paredit-backward-barf-sexp)
+        ([(control right)] . paredit-forward-slurp-sexp)
+        ([(control left)] . paredit-forward-barf-sexp)
         ([(control meta right)] . paredit-backward-barf-sexp)
-        ;; ([(escape control right)] . paredit-backward-barf-sexp)
+        ([(control meta left)] . paredit-backward-slurp-sexp)
         ("\M-r" . paredit-raise-sexp)
-        ("M-;" . paredit-comment-dwim)
+        ("\M-;" . paredit-comment-dwim)
         ; Miscellaneous Commands
         ("\M-S" . paredit-split-sexp)
         ("\M-J" . paredit-join-sexps)
         ("\C-c \C-\M-l" . paredit-recentre-on-sexp)
         ("\M-q" . paredit-reindent-defun))
   "Paredit keys to be available in all viper states.
-
 Stored as an alist of (key-str . func) pairs.")
-
 
 ;;; Functions for setting up paredit-viper-compat:
 
+;;;###autoload
 (defun paredit-viper-compat ()
   "Enable paredit keybindings for viper-mode.
 
@@ -175,7 +174,7 @@ on a per buffer basis."
   (viper-add-local-keys 'insert-state paredit-viper-compat-all-states-keys)
   (viper-add-local-keys 'vi-state paredit-viper-compat-all-states-keys)
   (viper-add-local-keys 'emacs-state
-                         paredit-viper-compat-all-states-keys))
+                        paredit-viper-compat-all-states-keys))
 
 (defun paredit-viper-add-local-keys (state alist)
   "Add custom keys to viper-mode when paredit is enabled.
@@ -187,13 +186,12 @@ Normally, this would be called from a hook to a major mode or
 on a per buffer basis.
 Usage:
   (paredit-viper-add-local-keys state '((key-str . func) (key-str . func)...))"
-  (eval-after-load "viper-mode"
-    (if (equal state 'all-states)
-        (progn
-          (viper-add-local-keys 'emacs-state alist)
-          (viper-add-local-keys 'vi-state alist)
-          (viper-add-local-keys 'insert-state alist))
-      (viper-add-local-keys state alist))))
+  (if (equal state 'all-states)
+      (progn
+        (viper-add-local-keys 'emacs-state alist)
+        (viper-add-local-keys 'vi-state alist)
+        (viper-add-local-keys 'insert-state alist))
+    (viper-add-local-keys state alist)))
 
 
 (provide 'paredit-viper-compat)
