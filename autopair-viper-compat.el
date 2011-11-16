02749ea338a23cd335bb55c1e74830267670f607
Martial <hondana@gmx.com>
2011-11-16 15:18:57 +0100

    autopair-viper-compat > autopair + viper + vimpulse | cua + vimpulse

--- autopair-viper-compat.el
+++ autopair-viper-compat.el
@@ -6,62 +6,19 @@
 (defun autopair-handle-viper (action pair pos-before)
   (unless (eq viper-current-state 'vi-state)
     (autopair-default-handle-action action pair pos-before)))
-(add-to-list 'autopair-handle-action-fns #'autopair-handle-viper)
 
 ;;; AUTOPAIR-BACKSPACE MUST precede 'VIPER-DEL-BACKWARD-CHAR-IN-INSERT
-;; (no hack found but this: http://stackoverflow.com/questions/6042070/how-to-use-vimpulse-together-with-autopair-el)
-(defun not-escaped (escape-char)
-  "Character immediately before cursor is not prefixed by escape-char"
-  (let ((count 0))
-    (save-excursion
-      (if (char-before)
-          (backward-char))
-      (while (and (char-before)
-                  (= (char-before) escape-char))
-        (setq count (+ count 1))
-        (backward-char))
-      (if (= 0
-             (% count 2))
-          t
-        nil))))
+;;  Idea: stephan-baumeister.com
+;;  Motivation: `autopair-mode' makes 'emulation-mode-map-alists to append 'autopair-emulation-alist
+(defadvice viper-change-state-to-insert (after autopair nil activate)
+  (add-to-ordered-list 'emulation-mode-map-alists 'autopair-emulation-alist 300))
 
-(defun in-matched-empty-pair (pair-list)
-  "tell if cursor is in an empty pair in pair-list"
-  (let ((next-char (char-after))
-        (prev-char (char-before))
-        (matched nil)
-        (pair)
-        (pair-left)
-        (pair-right))
-    (if (and next-char
-             prev-char)
-          (while (and (setq pair
-                       (pop pair-list))
-                      (not matched))
-            (setq pair-left (pop pair)
-                  pair-right (pop pair))
-            (if (= next-char pair-right)
-                (if (and
-                     (= prev-char pair-left)
-                     (not-escaped ?\\))
-                    (setq matched t)))))
-    (if matched
-        t
-      nil)))
+(eval-after-load "vimpulse"
+  '(progn
+     ;; viper keymaps in vimpulse-viper-function-redefinitions are pushed in front of the alist
+     (defadvice vimpulse-normalize-minor-mode-map-alist (after order-viper--key-maps nil activate)
+       (add-to-ordered-list 'emulation-mode-map-alists 'viper--key-maps 500)))) ; TODO: do the same in a CUA-viper-compat script
 
-(defvar viper-workaround-pairs
-      '(
-        (?\" ?\")
-        (?\' ?\')
-        (?\` ?\`)
-        (?\( ?\))
-        (?\[ ?\])
-        (?\{ ?\})
-        ))
-
-;; Workaround for integration problem with autopair
-(defadvice viper-del-backward-char-in-insert (before viper-auto-delete-pair-backward nil activate)
-  (if (in-matched-empty-pair viper-workaround-pairs)
-      (delete-char 1)))
+(add-to-list 'autopair-handle-action-fns #'autopair-handle-viper)
 
 (provide 'autopair-viper-compat)
